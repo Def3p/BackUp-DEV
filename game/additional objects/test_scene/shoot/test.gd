1a92e896adf790eac974365d54a63fa1549cf509
Def3P <122175474+3PuTeLb@users.noreply.github.com>
2023-08-15 13:21:33 +0400

    BackUp 0.1.1

--- game/additional objects/test_scene/shoot/test.gd
+++ game/additional objects/test_scene/shoot/test.gd
@@ -5,16 +5,18 @@ extends CharacterBody2D
 const BulletPath = preload("res://bullet.tscn")
 
 func _process(delta):
+	
 	if Input.is_action_just_pressed("shoot"):
+		$Node2D.look_at(get_global_mouse_position())
 		shoot()
 		
-		$Node2D.look_at(get_global_mouse_position())
+		
 
 func shoot():
 	var bullet = BulletPath.instantiate()
 	
 	get_parent().add_child(bullet)
 	bullet.position = $Node2D/Marker2D.global_position
-	
-	bullet.velocity = get_global_mouse_position() - bullet.position
 	bullet.rotation = piz.rotation
+	bullet.velocity = get_global_mouse_position() - bullet.position
+	
