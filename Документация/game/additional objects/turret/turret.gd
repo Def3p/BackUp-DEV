4068876b81a62235456e3afba87238fc896c526d
Def3P <122175474+3PuTeLb@users.noreply.github.com>
2023-08-17 20:29:03 +0400

    0.1.4

---
+++ Документация/game/additional objects/turret/turret.gd
@@ -0,0 +1,48 @@
+extends Node2D
+
+@onready var os = $rotair_tur
+@onready var on_off = $"on _ off"
+
+@onready var gun = $"rotair_tur/Turel-gun"
+@onready var raycast = $rotair_tur/RayCast2D
+@onready var kd = $rotair_tur/Timer
+var bul_tr = true
+var posit_pl = null
+
+const BulletPath = preload('res://game/additional objects/bullet/bullet.tscn')
+
+func _process(delta):
+	posit_pl = Global.ret_pos()
+	if global_transform.origin[0] > posit_pl[0]:
+		$rotair_tur/Marker2D.position[1] = -5
+		$"rotair_tur/Turel-gun".position[1] = -5
+		$rotair_tur/turret_hitbox.position[1] = -10
+		$rotair_tur/RayCast2D.position[1] = -5
+		$rotair_tur/GPUParticles2D.position[1] = -5
+		on_off.flip_h = true
+		gun.flip_v = true
+	if global_transform.origin[0] <= posit_pl[0]:
+		$rotair_tur/Marker2D.position[1] = 5
+		$"rotair_tur/Turel-gun".position[1] = 5
+		$rotair_tur/turret_hitbox.position[1] = 0
+		$rotair_tur/RayCast2D.position[1] = 5
+		$rotair_tur/GPUParticles2D.position[1] = 5
+		on_off.flip_h = false
+		gun.flip_v = false
+	$rotair_tur.look_at(posit_pl)
+	if raycast.get_collider() is Player:
+		shoot()
+
+func shoot():
+	if bul_tr:
+		$rotair_tur/GPUParticles2D.emitting = true
+		var bullet = BulletPath.instantiate()
+		get_parent().add_child(bullet)
+		bullet.position = $rotair_tur/Marker2D.global_position
+		bullet.rotation = os.rotation
+		bullet.dir = posit_pl - bullet.position
+		bul_tr = false
+		kd.start()
+	
+func _on_timer_timeout():
+	bul_tr = true
---
+++ Документация/game/player/pl_dash.gd
@@ -0,0 +1,83 @@
+extends CharacterBody2D
+
+class_name Player
+
+var SPEED = 170
+var norm_SPEED = 170
+var dash_SPEED = 1500
+
+var fast_fell = false
+var JUMP_VELOCITY = -280.0
+var jump_add = 1
+var jump_count = 0
+
+var right_left = true
+
+@onready var camera = $Camera2D
+@onready var anim_sprite = $AnimatedSprite2D
+@onready var dash = $dash
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+#func _ready():
+#	print(get_tree().get_nodes_in_group("Player")[0])
+
+func _physics_process(delta):
+	if Global.hp_input() < 1:
+		get_tree().reload_current_scene()
+		Global.regen(100)
+	Global.add_pos(global_transform.origin)
+	if Input.is_action_just_pressed("+"):
+		if camera.zoom[0] < 10 or camera.zoom[1] < 10:
+			camera.zoom[0] += 1
+			camera.zoom[1] += 1
+	if Input.is_action_just_pressed("-"):
+		if camera.zoom[0] != 0.5 or camera.zoom[1] != 0.5:
+			camera.zoom[0] -= 1
+			camera.zoom[1] -= 1
+	
+	if right_left:
+		anim_sprite.flip_h = true
+	else:
+		anim_sprite.flip_h = false
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	if Input.is_action_just_pressed("space") and jump_count < jump_add:
+		velocity.y = JUMP_VELOCITY
+		jump_count += 1
+	
+	if is_on_floor():
+		jump_count = 0
+		
+	if not is_on_floor():
+		if velocity.y > 0 and not fast_fell:
+			velocity.y += 100
+			fast_fell = true
+	else:
+		fast_fell = false
+		
+	var direction = Input.get_axis("left", "right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		
+	if velocity.x == 0:
+		anim_sprite.play("idle")
+	else:
+		if(velocity.x > 0):
+			right_left = false
+			anim_sprite.play("walk")
+		else:
+			right_left = true
+			anim_sprite.play("walk")
+	
+	if Input.is_action_just_pressed("shift"):
+		SPEED = dash_SPEED
+		dash.start()
+	move_and_slide()
+	
+func _on_dash_timeout():
+	SPEED = norm_SPEED
