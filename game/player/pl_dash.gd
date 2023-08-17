d0604fed06a34c56f69ab615fc5711e15bcce76c
catDok05 <wowapolyakow05@gmail.com>
2023-08-17 03:15:06 +0300

    двойной прыжок2

--- game/player/pl_dash.gd
+++ game/player/pl_dash.gd
@@ -1,15 +1,15 @@
+
 extends CharacterBody2D
 
 class_name Player
 
-
-
-
+var jump_count = 0
+var jump_max = 6
 var SPEED = 170.0
 var JUMP_VELOCITY = -250.0
 var DOUBLE_JUMP_VELOCITY = -200.0
 var fast_fell = false
-var can_double_jump = false
+var jumps_remaining = 2  # Track the remaining jumps
 
 var right_left = true
 
@@ -18,11 +18,9 @@ var right_left = true
 
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-#func _ready():
-#	print(get_tree().get_nodes_in_group("Player")[0])
-
 func _physics_process(delta):
 	Global.add_pos(global_transform.origin)
+	
 	if Input.is_action_just_pressed("+"):
 		if camera.zoom[0] < 10 or camera.zoom[1] < 10:
 			camera.zoom[0] += 1
@@ -39,38 +37,39 @@ func _physics_process(delta):
 	
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
-		# check for double jump
-		if  Input.is_action_pressed("space"):
+		
+		if jumps_remaining > 0 and Input.is_action_pressed("space"):
 			velocity.y = DOUBLE_JUMP_VELOCITY
-	else:
-		can_double_jump = true
-	
+			jumps_remaining -= 1
+			
+	if is_on_floor() and jump_count!=0:
+		jump_count = 0
+	if jump_count < jump_max:
+		jumps_remaining = 2
 		if Input.is_action_pressed("space"):
 			velocity.y = JUMP_VELOCITY
-			can_double_jump = false
+			jump_count +=1
 		
-	if not is_on_floor():
 		if velocity.y > 0 and not fast_fell:
-			velocity.y += 100
+			velocity.y += -100
 			fast_fell = true
 	else:
 		fast_fell = false
-		
+	
 	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
+	
 	if velocity.x == 0:
 		anim_sprite.play("idle")
 	else:
-		if(velocity.x > 0):
+		if velocity.x > 0:
 			right_left = false
 			anim_sprite.play("walk")
 		else:
 			right_left = true
 			anim_sprite.play("walk")
-
+	
 	move_and_slide()
